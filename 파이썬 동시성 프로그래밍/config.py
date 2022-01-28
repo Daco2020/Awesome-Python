faa533ca58e0ff659a3b38346ea68837c246129d
Daco <dacokim32@gmail.com>
2022-01-28 20:05:24 +0900

    wip

--- 파이썬 동시성 프로그래밍/config.py
+++
@@ -1,8 +0,0 @@
-naver_api_header = {
-    "X-Naver-Client-Id": "d7fdlpDnLDNd8ijtcVPg",
-    "X-Naver-Client-Secret": "3FpGQv4QAK"
-}
-
-kakao_api_header = {
-    "Authorization": "KakaoAK 095a23c1f53ae14df69dad9e4bf987c9"
-}
